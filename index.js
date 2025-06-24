67ecfd39ff7a868ed3bf354016a8f8b7a482dedd
lincolnngugi <lincolnngugi6@gmail.com>
2025-06-24 12:00:41 +0300

    CREATING AND INSERTING DOM

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the <main id="main"> element
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = 'Your Name is the champion'; 
